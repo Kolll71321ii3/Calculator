ba89b3e8c9865e234dc51c91255d3935b88a6434
Dmitriy <dmitriy.iv00@gmail.com>
2024-12-10 18:15:54 +0300

    update

--- src/main/java/org/example/calculator/HelloController.java
+++ src/main/java/org/example/calculator/HelloController.java
@@ -1,14 +1,25 @@
 package org.example.calculator;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 
-public class HelloController {
+import java.awt.event.ActionEvent;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelloController implements Initializable {
     @FXML
     private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    /*@FXML
+    public void onPlusClick() {
+        String str = welcomeText.getText();
+        welcomeText.setText(str + "+");
+    }*/
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        welcomeText.setText("");
     }
 }
