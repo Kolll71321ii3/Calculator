e7171c9d65b2e10233e430d2bf0bfe96421f478d
unknown <dva79@mail.ru>
2024-12-17 20:51:13 +0300

    buttons from vad

--- src/main/java/org/example/calculator/HelloController.java
+++ src/main/java/org/example/calculator/HelloController.java
@@ -18,6 +18,78 @@ public class HelloController implements Initializable {
         welcomeText.setText(str + "+");
     }
 
+    @FXML
+    public void onZeroClick() {
+        String str = welcomeText.getText();
+        welcomeText.setText(str + "0");
+    }
+
+    @FXML
+    public void onOneClick() {
+        String str = welcomeText.getText();
+        welcomeText.setText(str + "1");
+    }
+
+    @FXML
+    public void onTwoClick() {
+        String str = welcomeText.getText();
+        welcomeText.setText(str + "2");
+    }
+
+    @FXML
+    public void onThreeClick() {
+        String str = welcomeText.getText();
+        welcomeText.setText(str + "3");
+    }
+
+    @FXML
+    public void onFourClick() {
+        String str = welcomeText.getText();
+        welcomeText.setText(str + "4");
+    }
+
+    @FXML
+    public void onFiveClick() {
+        String str = welcomeText.getText();
+        welcomeText.setText(str + "5");
+    }
+
+    @FXML
+    public void onSixClick() {
+        String str = welcomeText.getText();
+        welcomeText.setText(str + "6");
+    }
+
+    @FXML
+    public void onPointClick() {
+        String str = welcomeText.getText();
+        welcomeText.setText(str + ",");
+    }
+
+    @FXML
+    public void onPercentClick() {
+        String str = welcomeText.getText();
+        welcomeText.setText(str + "%");
+    }
+
+    @FXML
+    public void onMinusClick() {
+        String str = welcomeText.getText();
+        welcomeText.setText(str + "-");
+    }
+
+    @FXML
+    public void onMultiplicationClick() {
+        String str = welcomeText.getText();
+        welcomeText.setText(str + "*");
+    }
+
+    @FXML
+    public void onExponentiationClick() {
+        String str = welcomeText.getText();
+        welcomeText.setText(str + "^");
+    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         welcomeText.setText("");
